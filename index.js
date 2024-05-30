6ec983528867dbccbf5d188aa033c5cc02c94ce9
Nightishes <q.bathany@gmail.com>
2024-05-30 18:08:25 +0200

    0.1 release

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const path = require("node:path");
 const { Client, Collection, GatewayIntentBits } = require("discord.js");
 const { token } = require("./config.json");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, "commands");
@@ -41,5 +47,45 @@ for (const file of eventFiles) {
     client.on(event.name, (...args) => event.execute(...args));
   }
 }
+
+client.on("messageCreate", (message) => {
+  let splitMessage = message.content.toLowerCase().split(" ");
+
+  const badWordsDogshit = ["dogshit", "garbage"];
+
+  const pizzaReminder = ["promise", "pizza"];
+
+  if (message.author.id === "1204195877208465421") {
+    if (
+      (splitMessage != 1 &&
+        splitMessage.some((x) => badWordsDogshit.includes(x))) ||
+      message.content.toLowerCase().includes("read a guide") ||
+      message.content.toLowerCase().includes("so bad at videogames") ||
+      message.content.toLowerCase().includes("just press 1-2-3") ||
+      message.content.toLowerCase().includes("just press 123") ||
+      message.content.toLowerCase().includes("utter garbage") ||
+      message.content.toLowerCase().includes("I am just average")
+    ) {
+      message.channel.send(
+        "https://cdn.discordapp.com/attachments/826368743197900810/1131255391607607387/MARTIN.jpg?ex=66594c6f&is=6657faef&hm=150ad387db06da69389c55116fbbb0b112930a060a8372e30dbc3642090d4d4d&"
+      );
+    }
+
+    if (
+      splitMessage != 1 &&
+      splitMessage.some((x) => pizzaReminder.includes(x))
+    ) {
+      message.channel.send(
+        "https://cdn.discordapp.com/attachments/772520066708799489/921469704357445632/Screenshot_20211217_193124.jpg?ex=66596ec9&is=66581d49&hm=a73144c1ba785f57a3eb71d8f2325637bccf09e6dce442442cf445b910902df9&"
+      );
+    }
+
+    if (splitMessage != 1 && splitMessage.includes("Nina")) {
+      message.channel.send(
+        "Proposal: Continue to provide care for this person"
+      );
+    }
+  }
+});
 
 client.login(token);

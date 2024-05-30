ca5087bcff5fde926985f972d05f976002fcdef3
Nightishes <q.bathany@gmail.com>
2024-05-30 21:23:25 +0200

    better find() version

--- index.js
+++ index.js
@@ -49,38 +49,42 @@ for (const file of eventFiles) {
 }
 
 client.on("messageCreate", (message) => {
-  let splitMessage = message.content.toLowerCase().split(" ");
+  let userMessage = message.content.toLowerCase();
 
-  const badWordsDogshit = ["dogshit", "garbage"];
+  const badWordsDogshit = [
+    "dogshit",
+    "garbage",
+    "i am bad",
+    "read a guide",
+    "so bad at videogames",
+    "just press 1-2-3",
+    "utter garbage",
+    "just press 123",
+    "I am just average",
+  ];
 
   const pizzaReminder = ["promise", "pizza"];
 
-  if (message.author.id === "1204195877208465421") {
-    if (
-      (splitMessage != 1 &&
-        splitMessage.some((x) => badWordsDogshit.includes(x))) ||
-      message.content.toLowerCase().includes("read a guide") ||
-      message.content.toLowerCase().includes("so bad at videogames") ||
-      message.content.toLowerCase().includes("just press 1-2-3") ||
-      message.content.toLowerCase().includes("just press 123") ||
-      message.content.toLowerCase().includes("utter garbage") ||
-      message.content.toLowerCase().includes("I am just average")
-    ) {
+  const ninaTrigger = ["nina", "fiancee", "minrin"];
+
+  const proposalCompliment = [
+    "Proposal: a word of appreciation is appropriate.",
+  ];
+
+  if (message.author.id === "235817429031321601") {
+    if (badWordsDogshit.find((x) => userMessage.includes(x))) {
       message.channel.send(
         "https://cdn.discordapp.com/attachments/826368743197900810/1131255391607607387/MARTIN.jpg?ex=66594c6f&is=6657faef&hm=150ad387db06da69389c55116fbbb0b112930a060a8372e30dbc3642090d4d4d&"
       );
     }
 
-    if (
-      splitMessage != 1 &&
-      splitMessage.some((x) => pizzaReminder.includes(x))
-    ) {
+    if (pizzaReminder.find((x) => userMessage.includes(x))) {
       message.channel.send(
         "https://cdn.discordapp.com/attachments/772520066708799489/921469704357445632/Screenshot_20211217_193124.jpg?ex=66596ec9&is=66581d49&hm=a73144c1ba785f57a3eb71d8f2325637bccf09e6dce442442cf445b910902df9&"
       );
     }
 
-    if (splitMessage != 1 && splitMessage.includes("Nina")) {
+    if (ninaTrigger.find((x) => userMessage.includes(x))) {
       message.channel.send(
         "Proposal: Continue to provide care for this person"
       );
